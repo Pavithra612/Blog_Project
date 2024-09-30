2ce2515e03e0bda56fe9b0f26f4b1dfe29a26efd
Pavithra K <kpavitra@dataseers.in>
2024-09-30 07:23:48 +0530

    initial commit

---
+++ frontend/src/Pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from './NavBar';
+
+const Home =() => {
+    return(
+        <div>
+            <NavBar/>
+            <p>Welcome to Home page !</p>
+        </div>
+    )
+}
+
+export default Home;
---
+++ frontend/src/Pages/NavBar.js
@@ -0,0 +1,48 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button,
+  Tabs,
+  Tab,
+  Stack
+} from "@mui/material";
+//import CreateIcon from '@mui/icons-material/Create';
+import { PiReadCvLogo } from "react-icons/pi";
+import { TbLogout } from "react-icons/tb";
+//import { FaBookReader } from "react-icons/fa";
+
+function NavBar() {
+  return (
+    <React.Fragment>
+    <AppBar position="static" sx={{background:"#19199e"}}>
+      <Toolbar>
+        
+        <PiReadCvLogo size="1.5em" />
+        
+        <Typography variant="h6" component="div" sx={{flexGrow :1}}>
+          K Pavithra
+        </Typography>
+        {/* <Stack direction='row' spacing={2}> */}
+        {/*<Box> */}
+        <Tabs  textcolor="white" value={0} indicatorColor="white">
+          <Tab style={{color:'white'}} label="Read Blogs"/>
+          <Tab style={{color:'white'}} label="Dashboard"/>
+          <Tab style={{color:'white'}} label="About Me"/>
+          </Tabs>
+          <IconButton edge="start" color="inherit" aria-label="logo">
+          <TbLogout size="2.1rem"/>
+          </IconButton>
+          {/* <Button color="inherit">Read Blogs</Button>
+          <Button color="inherit">DashBoard</Button>
+          <Button color="inherit">About Me</Button> */}
+        {/*</Box> */}
+        {/* </Stack> */}
+      </Toolbar>
+    </AppBar>
+    </React.Fragment>
+  );
+}
+export default NavBar;
